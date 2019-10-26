ea777a8cc94eeb73b87d6c2bac6c734b670f4d85
Oleksii Nelin <xsorter@gmail.com>
2019-10-27 00:40:33 +0300

    basic functionality added

--- task_networks/port_sniffer/program.js
+++ task_networks/port_sniffer/program.js
@@ -1,22 +1,45 @@
 const net = require("net");
+const args = require("minimist")(process.argv.slice(2));
+
+const range = args.ports.split("-");
+const firstPort = +range[0];
+const lastPort = +range[1];
+const openedPorts = [];
+
+const ports =
+  range.length === 1 ? [firstPort] : populateArray(firstPort, lastPort);
 
 const openedPortCheck = (host, port, callback) => {
   const server = net.createServer(socket => {
     socket.write("server");
     socket.pipe(socket);
   });
+
   server.listen(port, host);
-  server.on("error", e => {
-    callback(true);
-  });
-  server.on("listening", function(e) {
-    server.close();
+  server.on("error", err => {
     callback(false);
   });
-  console.log("host is", host);
-  console.log("port is", port);
+  server.on("listening", () => {
+    server.close();
+    callback(true);
+  });
 };
 
-openedPortCheck("127.0.0.1", 4200, returnVal => {
-  console.log(returnVal);
-});
+function populateArray(first, last) {
+  return Array.from({ length: last - first }, (_, k) => k + first);
+}
+
+async function showResult(ports) {
+  await ports.map(e => {
+    openedPortCheck(args.host, e, isOpened => {
+      if (isOpened) {
+        openedPorts.push(e);
+      }
+      console.log(isOpened, e);
+    });
+  });
+
+  console.log(`ports ${openedPorts.join()} are opened`);
+}
+
+showResult(ports);
