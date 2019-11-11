4df55ef26454e7017fad0718422f4598891b9245
xsorter <xsorter@gmail.com>
2019-11-11 16:57:33 +0200

    add errors resolve

--- task_networks/port_sniffer/program.js
+++ task_networks/port_sniffer/program.js
@@ -4,13 +4,20 @@ const args = require("minimist")(process.argv.slice(2));
 let range;
 
 if (args.ports) {
-  range = args.ports.toString().split("-");
+  if (args.ports.length) {
+    range = args.ports.toString().split("-");
+  } else {
+    process.stdout.write(
+      "Please provide a port number range or do not use --port parameter for default values (0-65535)"
+    );
+    process.exit(1);
+  }
 } else {
-  range = [0, 65500];
+  range = ["0", "65535"];
 }
 
 let firstPort = +range[0];
-const lastPort = +range[1];
+const lastPort = +range[1] ? +range[1] : +range[0];
 const openedPorts = [];
 
 const openedPortCheck = (host, port, callback) => {
@@ -21,7 +28,6 @@ const openedPortCheck = (host, port, callback) => {
   }, 300);
 
   socket.on("connect", () => {
-    console.log("CONNECTED");
     clearTimeout(time);
     socket.destroy();
     process.stdout.write(".");
@@ -29,8 +35,7 @@ const openedPortCheck = (host, port, callback) => {
     callback(true);
   });
   socket.on("error", function() {
-    console.log("ERROR");
-    clearTimeout(timer);
+    clearTimeout(time);
     callback(false);
   });
 };
@@ -44,26 +49,33 @@ const showResult = host => {
     --host - provide host IP adress or domain name \n
     Usage example: node program.js --ports 80-87 --host 172.217.3.110
     `);
-    return false;
+    process.exit(1);
   }
 
-  openedPortCheck(host, firstPort, function next() {
-    if (firstPort === lastPort) {
-      if (openedPorts.length) {
-        process.stdout.write(`\nports ${openedPorts.join()} are opened`);
-        return false;
-      } else {
-        process.stdout.write(`No opened ports was found`);
-        return false;
+  if (!args.host) {
+    process.stdout.write(
+      "Please provide host name. Type --help for usage manual."
+    );
+    process.exit(1);
+  } else {
+    openedPortCheck(host, firstPort, function next() {
+      if (firstPort === lastPort) {
+        if (openedPorts.length) {
+          process.stdout.write(`\nports ${openedPorts.join()} are opened`);
+          process.exit();
+        } else {
+          process.stdout.write(`No opened ports was found`);
+          process.exit(1);
+        }
       }
-    }
-    openedPortCheck(host, ++firstPort, next);
-  });
+      openedPortCheck(host, ++firstPort, next);
+    });
+  }
 };
 
 const ipLookup = () => {
   return new Promise((resolve, reject) => {
-    dns.lookup(args.host, (err, address, family) => {
+    dns.lookup(args.host, (err, address) => {
       if (err) reject(err);
       resolve(address);
     });
