769ccc0576307b4940200883acd766033891d767
Oleksii Nelin <xsorter@gmail.com>
2019-11-14 00:20:10 +0200

    code refactor

--- task_networks/port_sniffer/program.js
+++ task_networks/port_sniffer/program.js
@@ -1,149 +1,101 @@
-const mapLimit = require("async/mapLimit");
-
-const net = require("net");
-const dns = require("dns");
-const args = require("minimist")(process.argv.slice(2));
-const range = findPortsRange();
+const mapLimit = require('async/mapLimit')
+const net = require('net')
+const dns = require('dns')
+const args = require('minimist')(process.argv.slice(2))
+const range = findPortsRange()
 
 const ports = {
-  //fill list from range here
-  portList: [
-    79,
-    80,
-    81,
-    82,
-    83,
-    84,
-    85,
-    86,
-    87,
-    88,
-    89,
-    90,
-    91,
-    92,
-    93,
-    94,
-    95,
-    96,
-    97,
-    98,
-    99,
-    100,
-    101,
-    102,
-    103,
-    442,
-    443,
-    444,
-    445
-  ],
-  firstPort: +range[0],
-  lastPort: +range[1] ? +range[1] : +range[0],
+  portList: fillArrayRange(range[1] - range[0] + 1, range[0]),
+  firstPort: range[0],
+  lastPort: range[1] ? range[1] : range[0],
   openedPorts: []
-};
+}
 
-const openedPortCheck = (host, port, callback) => {
-  const socket = net.createConnection(port, host);
-  let time = socket.setTimeout(300, () => {
-    socket.destroy();
-    callback(false);
-  });
+const openedPortCheck = (host, port, checkCallback) => {
+  const socket = net.createConnection(port, host)
+  const time = socket.setTimeout(300, () => {
+    socket.destroy()
+    checkCallback(false)
+  })
 
-  socket.on("connect", () => {
-    clearTimeout(time);
-    socket.destroy();
-    process.stdout.write(".");
-    ports.openedPorts.push(port);
-    callback(true);
-  });
-  socket.on("error", function() {
-    clearTimeout(time);
-    callback(false);
-  });
-};
+  socket.on('connect', () => {
+    clearTimeout(time)
+    socket.destroy()
+    console.log('.')
+    ports.openedPorts.push(port)
+    checkCallback(true)
+  })
+  socket.on('error', function () {
+    clearTimeout(time)
+    checkCallback(false)
+  })
+}
 
 const showResult = host => {
   if (args.help) {
-    process.stdout.write(messages().help);
-    process.exit(1);
+    console.table(messages().help)
+    return false
   }
   if (!args.host) {
-    process.stdout.write(messages().noHost);
-    process.exit(1);
+    return false
   } else {
-    mapLimit(ports.portList, 20, async (e, i) => {
-      await openedPortCheck(host, e, () => {
-        if (e === 445) {
-          //if current == last port from list
+    mapLimit(ports.portList, 10, async currentPort => {
+      await openedPortCheck(host, currentPort, () => {
+        if (currentPort === ports.lastPort) {
           if (ports.openedPorts.length) {
-            process.stdout.write(
-              messages(ports.openedPorts.join()).openedPorts
-            );
-            process.exit();
+            console.table(messages(ports.openedPorts.join()).openedPorts)
+            process.exit(0)
           } else {
-            process.stdout.write(messages().portsNotFound);
-            process.exit(1);
+            console.table(messages().portsNotFound)
+            return false
           }
         }
-      });
-    });
-    /*openedPortCheck(host, ports.firstPort, function nextIteration() {
-      if (ports.firstPort === ports.lastPort) {
-        if (ports.openedPorts.length) {
-          process.stdout.write(messages(ports.openedPorts.join()).openedPorts);
-          process.exit();
-        } else {
-          process.stdout.write(messages().portsNotFound);
-          process.exit(1);
-        }
-      }
-      openedPortCheck(host, ++ports.firstPort, nextIteration);
-    });*/
-  }
-};
-
-function findPortsRange() {
-  if (args.ports) {
-    if (args.ports.length) {
-      return args.ports.toString().split("-");
-    } else {
-      process.stdout.write(messages().emptyPortsParameter);
-      process.exit(1);
-    }
-  } else {
-    return ["0", "65535"];
+      })
+    })
   }
 }
 
-function messages(openedPortsNumbers) {
+function findPortsRange () {
+  if (args.ports) {
+    if (args.ports.length) {
+      return args.ports.split('-').map(Number)
+    } else {
+      console.table(messages().emptyPortsParameter)
+      return false
+    }
+  } else {
+    return [0, 65535]
+  }
+}
+
+function messages (openedPortsNumbers) {
   return {
     help: `Port sniffer CLI tool. \n
     Parameters:
     --ports - type ports range
     --host - provide host IP adress or domain name \n
     Usage example: node program.js --ports 80-87 --host google.com`,
-    noHost: "Please provide host name. Type --help for usage manual.",
+    noHost: 'Please provide host name. Type --help for usage manual.',
     openedPorts: `\nports ${openedPortsNumbers} are opened`,
-    portsNotFound: "No opened ports was found",
+    portsNotFound: 'No opened ports was found',
     emptyPortsParameter:
-      "Please provide a port numbers range or skip --port parameter for default values (0-65535)"
-  };
+      'Please provide a port numbers range or skip --port parameter for default values (0-65535)'
+  }
 }
 
-function fillArrayRange(arrLength, firstVal) {
-  return Array.from(new Array(arrLength), (val, i) => i + firstVal);
+function fillArrayRange (arrLength, firstVal) {
+  return Array.from(new Array(arrLength), (_, i) => i + firstVal)
 }
 
 const ipLookup = () => {
   return new Promise((resolve, reject) => {
     dns.lookup(args.host, (err, address) => {
-      if (err) reject(err);
-      resolve(address);
-    });
-  });
-};
+      if (err) reject(err)
+      resolve(address)
+    })
+  })
+}
 
 ipLookup()
   .then(res => showResult(res))
-  .catch(() => process.stdout.write(`Adress not found`));
+  .catch(() => console.table('Adress not found'))
